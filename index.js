be5eb7b288eb1859011220755006dbd630e0e55b
henry van dusen <hgv2102@columbia.edu>
2015-05-13 18:59:35 -0400

    big commit

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path = require('path')
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, './', 'index.html'));
+});
+
+app.get('about', function (req, res) {
+  res.send('Henry Van Dusen <br> hgv2102');
+});
+
+app.listen(3300, function () {
+  console.log('hi')
+});
---
+++ public/js/app.js
@@ -0,0 +1,37 @@
+var app = angular.module('Homeworks',[])
+
+
+app.factory('Homework', function ($http) {
+    return [
+          {
+            assignment: 'Test First JavaScript',
+            url: 'http://github.com/myusername/TestFirst',
+            status: 'incomplete'
+          },
+          {
+            assignment: 'Angular Flash Cards Day 1',
+            url: 'http://github.com/myusername/angularday1',
+            status: 'completed'
+          },
+          {
+            assignment: 'Angular Flash Cards Day 2',
+            url: 'http://github.com/myusername/angularday2',
+            status: 'completed'
+          },
+          {assignment: 'Angular Flash Cards Day 3',
+          url: 'http://github.com/myusername/angular3',
+          status: 'completed'
+        }
+      ];
+});
+
+
+app.controller('MainController', function ($scope, Homework) {
+  $scope.homeworks = Homework
+  $scope.current = {}
+  $scope.show = function(thing){
+    $scope.current = thing
+    console.dir(thing)
+  }
+
+});
